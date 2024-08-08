8c6742d5eabf65285f28bfc3104fb589ee5e246b
Rakshith K <rakshith.k@bixbytessolutions.com>
2024-08-08 10:55:54 +0530

    666

--- helloRakshith.c
+++ helloRakshith.c
@@ -10,5 +10,5 @@
 #include "stdio.h"
 
 int main(int argc, char *argv[])
-  printf("Hello, rakshith0UiiJJPPP!I00IYTYT1177__FF_II_IIK_IIT111");
+  printf("Hello, rakshith0UiiJJPPP!I00IYTYT1177__FF_II_IIK_IIT111666");
 }
