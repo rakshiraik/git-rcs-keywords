964ae410ecb3bbe0c0892b71a22610564280d415
Rakshith K <rakshith.k@bixbytessolutions.com>
2024-08-08 16:34:14 +0530

    commentadded

--- hello.c
+++ hello.c
@@ -3,12 +3,13 @@
  * @author      @$Author: Rakshith K <rakshith.k@bixbytessolutions.com> $
  * @date        @$Date: Thu Aug 8 12:03:46 2024 $
  * @version     @$Revision: 08750ee5084963c2379cf9628e32074d2bf62e63 $
- *
+ * $Comment     @$Comment:		$
  * $Id: hello.c | Thu Aug 8 12:03:46 2024 | Rakshith K <rakshith.k@bixbytessolutions.com>  $
+ *%s
  */
 
 #include "stdio.h"
 
 int main(int argc, char *argv[])
-  printf("Hello, 55i world5587yy0jjLLLLLL!OOLLUUUUqqqqIIVVpp");
+  printf("Hello, 55i world5587yy0jjLLLLLL!OOLLUUUUqqqqIIVVssspp");
 }
