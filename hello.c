e8c89b367981468d59979fbf4a9e9f9e034b4e7c
Rakshith K <rakshith.k@bixbytessolutions.com>
2024-08-08 17:44:20 +0530

    SS

--- hello.c
+++ hello.c
@@ -11,5 +11,5 @@
 #include "stdio.h"
 
 int main(int argc, char *argv[])
-  printf("Hello, 55i world5587yy0jjLLLLLL!OOLLUUUUqqqqIIVVssssspp");
+  printf("Hello, 55i world5587yy0jjLLLLLL!OOLLUUUUqqqqIIVVSS");
 }
