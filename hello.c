01d9502daed1502f2a6134eccf9f789e112cb639
Rakshith K <rakshith.k@bixbytessolutions.com>
2024-08-08 11:38:45 +0530

    Update file headers with latest commit info

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
 /**
  * @file        hello.c
  * @author      @$Author: Rakshith K <rakshith.k@bixbytessolutions.com> $
- * @date        @$Date: Thu Aug 8 11:31:32 2024 $
- * @version     @$Revision: 3e3f83d205873cf9ff029b988029a9a75d8c2199 $
+ * @date        @$Date: Thu Aug 8 11:35:43 2024 $
+ * @version     @$Revision: 6a78e46bf0951d8cf152fa4213e4c9b0181dca4b $
  *
- * $Id: hello.c | Thu Aug 8 11:31:32 2024 | Rakshith K <rakshith.k@bixbytessolutions.com>  $
+ * $Id: hello.c | Thu Aug 8 11:35:43 2024 | Rakshith K <rakshith.k@bixbytessolutions.com>  $
  */
 
 #include "stdio.h"
