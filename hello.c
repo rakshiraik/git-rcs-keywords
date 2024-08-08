17a632c393e3542279e02c97bfbf82cd05708dde
Rakshith K <rakshith.k@bixbytessolutions.com>
2024-08-08 17:55:16 +0530

    hello

--- hello.c
+++ hello.c
@@ -3,13 +3,21 @@
  * @author      @$Author: Rakshith K <rakshith.k@bixbytessolutions.com> $
  * @date        @$Date: Thu Aug 8 12:03:46 2024 $
  * @version     @$Revision: 08750ee5084963c2379cf9628e32074d2bf62e63 $
-* $Comment:     $
  * $Id: hello.c | Thu Aug 8 12:03:46 2024 | Rakshith K <rakshith.k@bixbytessolutions.com>  $
- *%s
+ *
+ * @brief       This file contains a simple C program that prints a string to the console.
+ * @note        This is a basic example to demonstrate adding a comment header to a C file.
+ *
+ * Additional Information:
+ * - This program is for educational purposes.
+ * - It demonstrates the use of a simple `printf` statement.
+ *
  */
 
 #include "stdio.h"
 
 int main(int argc, char *argv[])
+{
   printf("Hello, 55i world5587yy0jjLLLLLL!OOLLUUUUqqqqIIVVSSHH");
 }
+
